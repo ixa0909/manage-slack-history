cb7d06a1f699a2be508125d1abf243ecbdf2a1c5
yabukazuki <yabuuchi@ss.cs.osakafu-u.ac.jp>
2022-11-28 02:24:58 +0900

    [help]getHistory

---
+++ backend/getHistory.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
+
+// type GetConversationHistoryParameters struct {
+// 	ChannelID          string
+// 	Cursor             string
+// 	Inclusive          bool
+// 	Latest             string
+// 	Limit              int
+// 	Oldest             string
+// 	IncludeAllMetadata bool
+// }
+
+func main() {
+
+	godotenv.Load("./.env")
+	TOKEN := os.Getenv("SLACK_BOT_TOKEN")
+	CHANNEL_ID := os.Getenv("CHANNEL_ID")
+
+	p := slack.GetConversationHistoryParameters{ChannelID: CHANNEL_ID, Cursor: "", Inclusive: true, Latest: "", Limit: 5, Oldest: "", IncludeAllMetadata: false}
+	fmt.Print(p.ChannelID)
+	api := slack.New(TOKEN)
+
+	history, err := api.GetConversationHistory(p)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Print(history)
+}

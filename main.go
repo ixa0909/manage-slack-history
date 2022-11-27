b71b6b471253c3d999d69bf0016913dd631f28bb
yabukazuki <yabuuchi@ss.cs.osakafu-u.ac.jp>
2022-11-27 23:41:32 +0900

    [add]getUserInfoをGoで

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	godotenv.Load("./.env")
+	TOKEN := os.Getenv("SLACK_BOT_TOKEN")
+	USER := os.Getenv("SLACK_USER")
+	fmt.Print(USER)
+	api := slack.New(TOKEN)
+	user, err := api.GetUserInfo(USER)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
+}

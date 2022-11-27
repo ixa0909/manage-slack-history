c6e7ae1ca36d69d5a30b98aab50c6c8a17a6abeb
yabukazuki <yabuuchi@ss.cs.osakafu-u.ac.jp>
2022-11-28 03:57:19 +0900

    [add]goで履歴を取得に成功

--- backend/getHistory.go
+++ backend/getHistory.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -8,6 +9,7 @@ import (
 	"github.com/slack-go/slack"
 )
 
+// 履歴を取る関数の引数の構造体定義部分
 // type GetConversationHistoryParameters struct {
 // 	ChannelID          string
 // 	Cursor             string
@@ -17,6 +19,17 @@ import (
 // 	Oldest             string
 // 	IncludeAllMetadata bool
 // }
+// 履歴を取る関数の返り値
+// type GetConversationHistoryResponse struct {
+// 	SlackResponse
+// 	HasMore          bool   `json:"has_more"`
+// 	PinCount         int    `json:"pin_count"`
+// 	Latest           string `json:"latest"`
+// 	ResponseMetaData struct {
+// 		NextCursor string `json:"next_cursor"`
+// 	} `json:"response_metadata"`
+// 	Messages []Message `json:"messages"`
+// }
 
 func main() {
 
@@ -24,7 +37,7 @@ func main() {
 	TOKEN := os.Getenv("SLACK_BOT_TOKEN")
 	CHANNEL_ID := os.Getenv("DEV_CHANNEL_ID")
 
-	p := slack.GetConversationHistoryParameters{ChannelID: CHANNEL_ID, Cursor: "", Inclusive: true, Latest: "", Limit: 5, Oldest: "", IncludeAllMetadata: true}
+	p := slack.GetConversationHistoryParameters{ChannelID: CHANNEL_ID, Cursor: "", Inclusive: true, Latest: "", Limit: 1, Oldest: "", IncludeAllMetadata: false}
 	// fmt.Print(p.ChannelID)
 	api := slack.New(TOKEN)
 
@@ -33,5 +46,8 @@ func main() {
 		fmt.Printf("%s\n", err)
 		return
 	}
-	fmt.Print(history)
+
+	// json にエンコードする必要があった
+	b,err:=json.Marshal(history)
+	fmt.Printf("%s\n", b)
 }
